0b8441f78bd53d9df3102d871c4cfcad5873bd98
“AurovD” <dmitry8891@mail.ru>
2023-03-23 16:01:30 +0300

    transition fixes test

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const primaryHeader = document.querySelector(".primary-header ");
 const navToggle = document.querySelector(".mobile-nav-toggle");
 const primaryNav = document.querySelector(".navigation");
+const navItem = document.querySelector(".nav-item");
 
 navToggle.addEventListener("click", () => {
     primaryNav.hasAttribute("data-visible") ?
@@ -9,3 +10,14 @@ navToggle.addEventListener("click", () => {
     primaryNav.toggleAttribute("data-visible");
     primaryHeader.toggleAttribute("data-overlay");
 });
+
+function isMobileDevice() {
+    return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
+};
+
+if (isMobileDevice()) {
+    alert("its mobile")
+} else {
+    console.log("its desktop")
+}
+
